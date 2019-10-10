8230e4aa856e08b06aea75d799a76ff99dd92574
Kathryn Schoephoester <kmschoep@usgs.gov>
2019-10-10 12:16:48 -0500

    Add function to return entire report type enum.
    add code checking plugins to pom, upgraded libs to remove high CVE
    vulnerabilities.

--- src/main/java/gov/usgs/aqcu/model/ReportParameterConfig.java
+++ src/main/java/gov/usgs/aqcu/model/ReportParameterConfig.java
@@ -4,19 +4,23 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ReportParameterConfig {
     
-    GW_VRSTAT("gwvrstatreport", Arrays.asList(
+    GW_VRSTAT("gwvrstatreport", "GW Visit Review Status", Arrays.asList(
     	new ReportBasicParameter("locationIdentifier", "Primary Location", "location")
     ));
 
 	private String reportType;
+	private String reportTypeDisplayName;
 	private List<ReportBasicParameter> parameters;
 	
-    ReportParameterConfig(String reportType, List<ReportBasicParameter> parameters) {
+    ReportParameterConfig(String reportType, String reportTypeDisplayName, List<ReportBasicParameter> parameters) {
         this.reportType = reportType;
+        this.reportTypeDisplayName = reportTypeDisplayName;
         this.parameters = parameters;
     }
 
@@ -28,10 +32,18 @@ public enum ReportParameterConfig {
     	return this.parameters;
     }
 
-    public static ReportParameterConfig getByReportType(String reportType) {
+    public String getReportTypeDisplayName() {
+		return reportTypeDisplayName;
+	}
+
+	public static ReportParameterConfig getByReportType(String reportType) {
         for(ReportParameterConfig config : values()) {
             if(config.getReportType().equals(reportType)) return config; 
         }
         return null;
     }
+	
+	public static String getReportTypes() throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(ReportParameterConfig.values());
+	}
 }
