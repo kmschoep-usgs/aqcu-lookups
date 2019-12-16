4da7ead3f3c523605f869851b35240ec467e2ff5
Kathryn Schoephoester <kmschoep@usgs.gov>
2019-12-16 09:31:29 -0600

    Allow DELETE method in spring security

--- src/main/java/gov/usgs/aqcu/config/WebMvcConfig.java
+++ src/main/java/gov/usgs/aqcu/config/WebMvcConfig.java
@@ -20,7 +20,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
 	}
 
 	@Override
