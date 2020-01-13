71fb97a22199b35967a44e99e4f431866e324281
Zack Moore <zmoore@usgs.gov>
2020-01-13 15:13:23 -0600

    Updated to latest Spring Boot. Fixed CRITICAL and HIGH vulnerabilities.

--- src/test/java/gov/usgs/aqcu/controller/ConfigControllerMVCTest.java
+++ src/test/java/gov/usgs/aqcu/controller/ConfigControllerMVCTest.java
@@ -32,6 +32,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -51,7 +52,8 @@ import gov.usgs.aqcu.model.config.SavedReportConfiguration;
 import gov.usgs.aqcu.reports.ReportConfigsService;
 
 @RunWith(SpringRunner.class)
-@AutoConfigureMockMvc(secure = false)
+@WithMockUser
+@AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 public class ConfigControllerMVCTest {
